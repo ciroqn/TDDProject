f3d2bfd6d933ff7ccb391e104d9bca234b380049
ciroqn <79422987+ciroqn@users.noreply.github.com>
2021-06-08 17:22:56 +0100

    Exercise setup
    We setup up the exercise in which a user navigates to root ('/') and sets value in the various input tags in html file. For example, the value set in 
    <textarea> is the content saves in the 'quote' variable. The behaviour is then finished off with a 'click' response. Note that the 'browser' allows the developer to simulate
    the behaviour of a user.

--- test.js
+++ test.js
@@ -8,6 +8,13 @@ describe('User visits project root', () => {
       const quote = 'I dwell in and around the sea, for all time, ship or no ship; I pervade the salty air, the waves, the dark.';
       const attributed = 'Davy Jones';
       const source = "Davy Jones' diary";
+      
+      // Exercise
+      browser.url('/');
+      browser.setValue('textarea[id=quote]', quote);
+      browser.setValue('input[id=attributed]', attributed);
+      browser.setValue('input[id=source]', source);
+      browser.click('input[type=submit]');
 
     
       
