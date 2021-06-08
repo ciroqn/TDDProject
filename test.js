a8b814e75976494d6d957c2474b1bcf90a46396d
ciroqn <79422987+ciroqn@users.noreply.github.com>
2021-06-08 17:18:52 +0100

    Setup up 'describes' and nest 'it'
    Writing skeleton to simulate user entering root/homepage and writing quote

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
+const {assert} = require('chai');
 
+const {assert} = require('chai');
+
+describe('User visits project root', () => {
+  describe('posting a quote', () => {
+    it('saves quote and metadata submitted by user', () => {
+
+     
+
+    
+      
+      
+    });
+  });
+});
