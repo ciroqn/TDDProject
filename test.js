eb724c6a8700b96736e9f3512df721916fe22a38
ciroqn <79422987+ciroqn@users.noreply.github.com>
2021-06-08 17:20:13 +0100

    Setup intitialise
    Creating three variables which makes up user behaviour

--- test.js
+++ test.js
@@ -1,12 +1,13 @@
 const {assert} = require('chai');
 
-const {assert} = require('chai');
-
 describe('User visits project root', () => {
   describe('posting a quote', () => {
     it('saves quote and metadata submitted by user', () => {
-
-     
+      
+      // Setup
+      const quote = 'I dwell in and around the sea, for all time, ship or no ship; I pervade the salty air, the waves, the dark.';
+      const attributed = 'Davy Jones';
+      const source = "Davy Jones' diary";
 
     
       
